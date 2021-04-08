3586e2bcc71e2b0c0982c901640a1567cb96f8ef
Nathan Stchepinsky <nathanstchepinsky@protonmail.com>
2021-04-08 18:24:17 +0200

    Swift string

---
+++ source_code/String/JSON/JSON.swift
@@ -0,0 +1,39 @@
+//
+//  String.swift
+//
+//  Created by Nathan on 30/07/2019.
+//  Copyright © 2019 Nathan. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+        func toJSON() -> [String] {
+            guard let data = self.data(using: .utf8, allowLossyConversion: false) else { return [] }
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String] {
+                    return json
+                }
+            } catch {
+               return[""]
+            }
+            return[""]
+        }
+    /// Convert a json string to a dictionnary [*String*:*String*]
+    func jsonToDictionary() -> [String: String]? {
+        print("[*] jsonToDictionnary : text given = \(self)")
+        if let data = self.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: String]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
+    
+    /// Enable str[3] to return the 3rd char of a string
+    subscript(offset: Int) -> Character {
+        self[index(startIndex, offsetBy: offset)]
+    }
+}
